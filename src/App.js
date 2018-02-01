9babd2cec3e9c963e1361f69ce3f0f103e605d5f
Pablo Pesce <papesce@gmail.com>
2018-02-01 17:39:41 -0300

    sample workign with css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import MediaQuery from "react-responsive";
+import Nav from "./components/Nav"
+
 
 class App extends Component {
   render() {
@@ -15,27 +16,9 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <div>Device Test</div>
-        <MediaQuery query='(min-device-width: 1224px)'>
-          <div>You are a desktop or laptop</div>
-          <MediaQuery query='(min-device-width: 1824px)'>
-            <div>You also have a huge screen</div>
-          </MediaQuery>
-          <MediaQuery query='(max-width: 1224px)'>
-            <div>You are sized like a tablet or mobile phone though</div>
-          </MediaQuery>
-        </MediaQuery>
-        <MediaQuery query='(max-device-width: 1224px)'>
-          <div>You are a tablet or mobile phone</div>
-        </MediaQuery>
-        <MediaQuery query='(orientation: portrait)'>
-          <div>You are portrait</div>
-        </MediaQuery>
-        <MediaQuery query='(orientation: landscape)'>
-          <div>You are landscape</div>
-        </MediaQuery>
-        <MediaQuery query='(min-resolution: 2dppx)'>
-          <div>You are retina</div>
-        </MediaQuery>
+        <navbar>
+          <Nav/>
+         </navbar>
       </div>
     );
   }
---
+++ src/components/Nav.jsx
@@ -0,0 +1,36 @@
+import React, {Component} from "react"
+import "./Nav.css"
+
+
+class Nav extends Component {
+  burgerToggle = ()  => {
+    let linksEl = document.querySelector('.narrowLinks');
+      if (linksEl.style.display === 'block') {
+                linksEl.style.display = 'none';
+            } else {
+                linksEl.style.display = 'block';
+            }
+  } 
+  render() {
+       return  (<nav>
+         <div className="navWide">
+          <div className="wideDiv">
+           <a href="#">Link 1</a>
+           <a href="#">Link 2</a>
+           <a href="#">Link 3</a>
+         </div>
+        </div>
+        <div className="navNarrow">
+          <i className="fa fa-bars fa-2x" onClick={this.burgerToggle}></i>
+           <div className="narrowLinks">
+             <a href="#">Link 1</a>
+             <a href="#">Link 2</a>
+             <a href="#">Link 3</a>
+          </div>
+        </div> 
+     </nav>)
+   }  
+}
+
+
+export default Nav
